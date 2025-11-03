2824cc63e52b2e6e0cded4733d85da97a76580ae
Peter Wegmann <peter.wegmann@yahoo.de>
2025-11-03 16:17:02 +0000

    Improve basic_swap implementation

--- crates/transpiler/src/passes/basic_swap/route.rs
+++ crates/transpiler/src/passes/basic_swap/route.rs
@@ -3,8 +3,9 @@ use pyo3::prelude::*;
 
 use qiskit_circuit::dag_circuit::{DAGCircuit, NodeType};
 use qiskit_circuit::{VarsMode};
-use qiskit_circuit::operations::{Operation};
-use qiskit_circuit::{PhysicalQubit};
+use qiskit_circuit::operations::{Operation, StandardGate};
+use qiskit_circuit::{PhysicalQubit, Qubit, VirtualQubit};
+use qiskit_circuit::packed_instruction::PackedInstruction;
 
 use std::convert::Infallible;
 
@@ -12,7 +13,7 @@ use crate::target::{Target};
 use crate::passes::sabre::route::{PyRoutingTarget};
 
 use rustworkx_core::dictmap::*;
-use rustworkx_core::shortest_path::{dijkstra, distance_matrix};
+use rustworkx_core::shortest_path::{dijkstra};
 
 /// Run basic swap on a circuit
 ///
@@ -26,12 +27,12 @@ pub fn basic_routing(
     target: &Target
 ) -> PyResult<Option<DAGCircuit>> {
 
-    //println!("{} days", 31);
+    println!("Starting");
 
     // Construct dag builder
-    let new_dag = dag.copy_empty_like(VarsMode::Alike)?;
+    let new_dag_copy = dag.copy_empty_like(VarsMode::Alike)?;
     //let mut new_dag = new_dag.into_builder();
-    let new_dag = new_dag.into_builder();
+    let mut new_dag = new_dag_copy.into_builder();
 
     // Compute distance matrix
     let routing_target = PyRoutingTarget::from_target(target)?;
@@ -41,97 +42,139 @@ pub fn basic_routing(
         return Ok(Some(dag.clone()));
     };
 
+    // TODO: Generate layout for mapping virtual to physical qubits
+    /* 
     for node in dag.topological_op_nodes()? {
 
         if let NodeType::Operation(inst) = &dag[node] {
             
 
             if inst.op.num_qubits() > 1 {
+                //println!("two-qubit gate");
 
                 // Calculate distance
-
+                // Get qubits and convert to physical
                 let qargs = dag.get_qargs(inst.qubits);
-                let index0 = qargs[0].index();
-                let index1 = qargs[1].index();
+                let index0_n = qargs[0].index() as u32;
+                let index1_n = qargs[1].index() as u32;
+                let index0 = PhysicalQubit::new(index0_n); //VirtualQubit::new(index0_n).to_phys(&layout);
+                let index1 = PhysicalQubit::new(index1_n); //VirtualQubit::new(index1_n).to_phys(&layout); 
+                //let a = layout.virt_to_phys[qargs[0].index()]
+                //let b = layout.virt_to_phys[qargs[1].index()]
 
                 // Calculate physical distance using coupling map
                 let dist = &target.distance;
-                let distance = dist[[index0, index1]];
+                let distance = dist[[index0.index(), index1.index()]];
 
-                println!("{}", distance);
+                //println!("{}", distance);
                 
                 if distance > 1.0 {
-                     
+                    /*
                     // Calculate shortest path between two qubit using dijkstra algorithm from rustworkx 
                     let shortest_path = {
                         let mut shortest_paths: DictMap<PhysicalQubit, Vec<PhysicalQubit>> = DictMap::new();
+
                         (dijkstra(
                             &target.neighbors,
-                            qargs[0],
-                            Some(qargs[1]),
+                            index0,
+                            Some(index1),
                             |_| Ok(1.),
                             Some(&mut shortest_paths),
                         ) as Result<Vec<_>, Infallible>)
                             .expect("error is infallible");
+                        
+                        // Return path from source to target, by removing source from the local shortest path and
+                        // returning it (swap_remove fn of Vec struct).
                         shortest_paths
-                            .swap_remove(&qargs[1])
+                            .swap_remove(&index1)
                             .expect("target is required to be connected")
                     };
 
-                    println!("{}", shortest_path)
-                    /*
-                
                     // Add swaps along the shortest path. Divide path in half (swap from target to middle of path, as well as target
                     // to middle of path), in order to reduce depth of circuit.
                     let split: usize = shortest_path.len() / 2;
-                    current_swaps.reserve(shortest_path.len() - 2);
+                    //current_swaps.reserve(shortest_path.len() - 2);
                     for i in 0..split {
-                        current_swaps.push([shortest_path[i], shortest_path[i + 1]]);  
-                    }
-                    for i in 0..split - 1 {
-                        let end = shortest_path.len() - 1 - i;
-                        current_swaps.push([shortest_path[end], shortest_path[end - 1]]);
-                    }
-
-                    // Apply swaps on the dag
-                    let apply_swap = |swap: &[PhysicalQubit; 2],
-                                    layout: &mut NLayout,
-                                    dag: &mut DAGCircuitBuilder|
-                    -> PyResult<NodeIndex> {
-                        layout.swap_physical(swap[0], swap[1]);
+                        //current_swaps.push([shortest_path[i], shortest_path[i + 1]]);  
+                        //println!("{}", shortest_path[i].0);
+                        //println!("{}", shortest_path[i+1].0);
                         let swap = PackedInstruction::from_standard_gate(
                             StandardGate::Swap,
                             None,
-                            dag.insert_qargs(&[Qubit(map_fn(swap[1]).0), Qubit(map_fn(swap[0]).0)]),
+                            new_dag.insert_qargs(&[Qubit(shortest_path[i].0), Qubit(shortest_path[i+1].0)]),
                         );
-                        dag.push_back(swap)
-                    };
-                    current_swaps.iter().for_each(|&swap| self.apply_swap(swap));
-                    */
+                        let _ = new_dag.push_back(swap);
+                    }
+                    
+                    for i in 0..split - 1 {
+                        let end = shortest_path.len() - 1 - i;
+                        // current_swaps.push([shortest_path[end], shortest_path[end - 1]]);
+                        //println!("{}", shortest_path[end].0);
+                        //println!("{}", shortest_path[end-1].0);
+
+                        let swap = PackedInstruction::from_standard_gate(
+                            StandardGate::Swap,
+                            None,
+                            new_dag.insert_qargs(&[Qubit(shortest_path[i].0), Qubit(shortest_path[i+1].0)]),
+                        );
+                        let _ = new_dag.push_back(swap);
+                    }
+                    
+                    let new_op = PackedInstruction::from_standard_gate(
+                        (inst.op).standard_gate(),
+                        None,
+                        new_dag.insert_qargs(&[Qubit(shortest_path[split-1].0), Qubit(shortest_path[split].0)]),
+                    );
+                    let _ = new_dag.push_back(new_op);
+                    // TODO: Change qubits, since we swapped
+                    // let _ = new_dag.push_back(inst.clone());
+
+
+                    // TODO: Swap back
+
+                    for i in (0..split).rev() {
+                        let swap = PackedInstruction::from_standard_gate(
+                            StandardGate::Swap,
+                            None,
+                            new_dag.insert_qargs(&[Qubit(shortest_path[i].0), Qubit(shortest_path[i+1].0)]),
+                        );
+                        let _ = new_dag.push_back(swap);
+                    }
+                    
+                    for i in (0..split - 1).rev() {
+                        let end = shortest_path.len() - 1 - i;
+                        // current_swaps.push([shortest_path[end], shortest_path[end - 1]]);
+                        //println!("{}", shortest_path[end].0);
+                        //println!("{}", shortest_path[end-1].0);
+
+                        let swap = PackedInstruction::from_standard_gate(
+                            StandardGate::Swap,
+                            None,
+                            new_dag.insert_qargs(&[Qubit(shortest_path[i].0), Qubit(shortest_path[i+1].0)]),
+                        );
+                        let _ = new_dag.push_back(swap);
+                    }
+                     */
 
                 } else {
                     // Swapping not needed! Add the gate to the dag
                     // TODO: Add gate to dag
+                    let _ = new_dag.push_back(inst.clone());
                 }
             } else {
                 // one-qubit gate. Add gate to the dag
                 // TODO: Add gate to dag
+                //println!("single qubit gate");
+                let _ = new_dag.push_back(inst.clone());
+
             }
         } else {
             unreachable!("Op nodes contain a non-operation");
         }   
     }
 
-
-    // TODO: qubits
-
-    
-    
-    // TODO: current_swaps
-
-
-
-
+    */
+    println!("Done");
 
     // Ok(new_dag)
     Ok(Some(new_dag.build()))
